560b1b4f9b6faf5ee4f2f40372584631c3ec9170
Sebastian Sibelle <sebastian@chuffed.org>
2016-05-26 09:13:04 +1000

    Added initial js files

---
+++ js/components/GistAddForm.js
@@ -0,0 +1,31 @@
+var GistAddForm = React.createClass({
+
+    getInitialState: function(){
+        return {
+            text: ''
+        }
+    },
+
+    onChange: function(e){
+        this.setState({ text: e.target.value });
+    },
+
+    addGist: function(e){
+        e.preventDefault();
+
+        this.props.onAdd(this.state.text)
+        this.setState({ text: '' })
+    },
+
+    render() {
+        return (
+            <div>
+                <form className="form-horizontal" onSubmit={this.addGist}>
+                    <input value={this.state.text} onChange={this.onChange} placeholder="Type a GitHub username..." />
+                    <button>Fetch Latest Gist</button>
+                </form>
+            </div>
+        );
+    }
+
+});
